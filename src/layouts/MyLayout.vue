b6175bede62b7df553d560febc423de4451fc919
unknown <wandersonwallison@gmail.com>
2018-11-28 23:29:57 -0300

    tables no agendamento

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -44,7 +44,24 @@
         <q-tab name="alarms" slot="title" icon="alarm"  />
         <q-tab name="movies" slot="title" icon="movie"  />
 
-        <q-tab-pane name="mails">Emails tab</q-tab-pane>
+        <q-tab-pane name="mails">
+          <q-table
+              :data="tableData"
+              :columns="columns"
+              selection="multiple"
+              :selected.sync="selectedSecond"
+              row-key="name"
+              color="secondary"
+              title="Select some rows"
+          >
+            <template slot="top-selection" slot-scope="props">
+                <q-btn color="secondary" flat label="Action 1" class="q-mr-sm" />
+                <q-btn color="secondary" flat label="Action 2" />
+                <div class="col" />
+               <q-btn color="negative" flat round icon="delete" @click="deleteRow" />
+           </template>
+      </q-table>
+        </q-tab-pane>
         <q-tab-pane name="alarms">Alarms tab</q-tab-pane>
         <q-tab-pane name="movies">Movies tab</q-tab-pane>
       </q-tabs>
@@ -61,11 +78,74 @@ export default {
   name: 'MyLayout',
   data () {
     return {
-      leftDrawerOpen: this.$q.platform.is.desktop
+      leftDrawerOpen: this.$q.platform.is.desktop,
+      columns: [{
+        name: 'desc',
+        required: true,
+        label: 'Dessert (100g serving)',
+        align: 'left',
+        field: 'name',
+        sortable: true
+      },
+      { name: 'calories', label: 'Calories', field: 'calories', sortable: true },
+      { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+      { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+      { name: 'protein', label: 'Protein (g)', field: 'protein' },
+      { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
+      { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
+      { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+      ],
+      filter: '',
+      visibleColumns: ['desc', 'fat', 'carbs', 'protein', 'sodium', 'calcium', 'iron'],
+      separator: 'horizontal',
+      selection: 'multiple',
+      selected: [
+        // initial selection
+        { name: 'Ice cream sandwich' }
+      ],
+      pagination: {
+        page: 2
+      },
+      paginationControl: { rowsPerPage: 3, page: 1 },
+      loading: false,
+      dark: true,
+      selectedSecond: [
+        { name: 'Eclair' }
+      ]
+    }
+  },
+  watch: {
+    'paginationControl.page' (page) {
+      this.$q.notify({
+        color: 'secondary',
+        message: `Navigated to page ${page}`,
+        actions: page < 4
+          ? [{
+            label: 'Go to last page',
+            handler: () => {
+              this.paginationControl.page = 4
+            }
+          }]
+          : null
+      })
+    }
+  },
+  computed: {
+    tableClass () {
+      if (this.dark) {
+        return 'bg-black'
+      }
     }
   },
   methods: {
-    openURL
+    openURL,
+    deleteRow () {
+      this.$q.notify({
+        color: 'secondary',
+        icon: 'delete',
+        message: `Will delete the selected row${this.selectedSecond.length > 1 ? 's' : ''} later, ok?`
+      })
+    }
   }
 }
 </script>
