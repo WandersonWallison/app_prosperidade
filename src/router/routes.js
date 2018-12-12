dd535d1289d827afbd98edc6d18df6a2bbdfa2fe
unknown <wanderson.rocha@oseusonho.com.br>
2018-12-12 16:50:26 -0300

    routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -15,7 +15,25 @@ const routes = [
     ]
   }
 ]
-
+/* routes.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const authUser = window.localStorage.getItem('Usuario')
+    const authUser2 = JSON.parse(authUser)
+    if (authUser) {
+      if (to.meta.permissions === true && authUser2.id_profile === 3) {
+        next()
+      } else {
+        next('/')
+        window.localStorage.clear()
+      }
+    } else {
+      next('/')
+      this.$router.push('/')
+      window.localStorage.clear()
+    }
+  }
+  next()
+}) */
 // Always leave this as last one
 if (process.env.MODE !== 'ssr') {
   routes.push({
