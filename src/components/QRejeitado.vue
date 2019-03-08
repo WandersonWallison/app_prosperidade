7cc628c9732d2214db11e564a0d09457536fafa6
unknown <wanderson.rocha@oseusonho.com.br>
2019-03-08 16:49:34 -0300

    alter form rejeitado

--- src/components/QRejeitado.vue
+++ src/components/QRejeitado.vue
@@ -27,13 +27,62 @@ export default {
       },
       selectHoras: [
         {
-          label: 'TESTE',
-          value: '1'
+          label: 'Não gostou da proposta',
+          value: 'Não gostou da proposta'
         },
         {
-          label: 'VAI ',
-          value: '2'
+          label: 'Achou os custos altos',
+          value: 'Achou os custos altos'
+        },
+        {
+          label: 'Não conhece a intituição',
+          value: 'Não conhece a intituição'
+        },
+        {
+          label: 'Tem proposta melhor do concorrente',
+          value: 'Tem proposta melhor do concorrente'
+        },
+        {
+          label: 'Não conhece o escritório',
+          value: 'Não conhece o escritório'
+        },
+        {
+          label: 'Experiência negativa anterior',
+          value: 'Experiência negativa anterior'
+        },
+        {
+          label: 'Não houve empatia',
+          value: 'Não houve empatia'
+        },
+        {
+          label: 'Gostaria de mais informações',
+          value: 'Gostaria de mais informações'
+        },
+        {
+          label: 'Pediu tempo para avaliar',
+          value: 'Pediu tempo para avaliar'
+        },
+        {
+          label: 'Peseou ser outra coisa',
+          value: 'Peseou ser outra coisa'
+        },
+        {
+          label: 'Vai ouvir opinião de familiar',
+          value: 'Vai ouvir opinião de familiar'
+        },
+        {
+          label: 'vai ouvir ipinião de colaborador, assessor ou consultor',
+          value: 'vai ouvir ipinião de colaborador, assessor ou consultor'
+        },
+        {
+          label: 'Já possui mesmo tipo de relacionamento com a XP',
+          value: 'Já possui mesmo tipo de relacionamento com a XP'
+        },
+        {
+          label: 'Já possui mesmo tipo de relacionamento em outra instituição',
+          value: 'Já possui mesmo tipo de relacionamento em outra instituição'
         }
+
       ]
     }
   },
